475829e191a969f0890ad7e33d7af099eea821da
wbrunovieira <wbrunovieira77@gmail.com>
2020-08-20 17:20:26 +0100

    Hello World C#

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, what's your name? ");
+
+            string name = Console.ReadLine();
+
+            Console.WriteLine("Hello" + "  " + name);
+        }
+    }
+}
